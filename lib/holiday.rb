eabaacfc23cbedf50b8102760fc5493949d62ec8
Joseph Senders <jasenders@gmail.com>
2018-08-13 17:05:44 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,6 +20,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # return the second element in the 4th of July array
     holiday_supplies[:summer][:fourth_of_july][1]
   end
+  end
 
 end
 
@@ -64,10 +65,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
