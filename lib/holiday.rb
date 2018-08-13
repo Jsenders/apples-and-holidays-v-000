9cabc30c4291d94cdfc5b54a265d9490934c0470
Joseph Senders <jasenders@gmail.com>
2018-08-13 17:26:10 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,8 +60,10 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 holiday_hash.each do |season, holiday|
   puts "#{season.to_s.capitalize}"
-  
+
   holiday.each do |holiday, activities|
+end
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
