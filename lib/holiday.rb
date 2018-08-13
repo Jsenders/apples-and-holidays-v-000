e564b456f8c0f46b08fdc559337c389c599538e3
Joseph Senders <jasenders@gmail.com>
2018-08-13 17:24:39 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,6 +63,7 @@ holiday_hash.each do |season, data|
     puts "#{holiday}: #{data}"
   end
 end
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
